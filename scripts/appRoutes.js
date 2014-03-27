bb6da8492faffc51579206b4bdddefb089c866d6
Sergei Yakimchik <siarhei_yakimmchyk@dancosoft.com>
2014-03-27 15:23:44 +0300

    modified ui (added new delivery page, modified index page)

--- scripts/appRoutes.js
+++ scripts/appRoutes.js
@@ -1,1 +1,32 @@
-define([],function(){return{defaultRoutePath:"/",defaultDependencies:["controller/MainController","controller/MenuController","service/AlertService","service/CurrencyService"],routes:{"/index":{templateUrl:"views/index.html",dependencies:["controller/IndexController"]},"/catalog/:type":{templateUrl:"views/catalog.html",dependencies:["controller/CatalogController","service/ProductService"]},"/product/:type/:id":{templateUrl:"views/product.html",dependencies:["controller/ProductController","service/OrderService","service/ProductService"]},"/interesting":{templateUrl:"views/interesting.html",dependencies:[]},"/about":{templateUrl:"views/about.html",dependencies:[]},"/feedback":{templateUrl:"views/feedback.html",dependencies:[]}}}})
+define([], function () {
+    return {
+        defaultRoutePath: "/",
+        defaultDependencies: ["controller/MainController", "controller/MenuController", "service/AlertService", "service/CurrencyService"],
+        routes: {
+            "/index": {
+                templateUrl: "views/index.html",
+                dependencies: ["controller/IndexController", "service/ProductService"]
+            },
+            "/catalog/:type": {
+                templateUrl: "views/catalog.html",
+                dependencies: ["controller/CatalogController", "service/ProductService"]
+            },
+            "/product/:type/:id": {
+                templateUrl: "views/product.html",
+                dependencies: ["controller/ProductController", "service/OrderService", "service/ProductService"]
+            },
+            "/interesting": {
+                templateUrl: "views/interesting.html",
+                dependencies: []
+            },
+            "/about": {
+                templateUrl: "views/about.html",
+                dependencies: []
+            },
+            "/feedback": {
+                templateUrl: "views/feedback.html",
+                dependencies: []
+            }
+        }
+    }
+})
--- scripts/controller/CatalogController.js
+++ scripts/controller/CatalogController.js
@@ -1,1 +1,36 @@
-define(["appModule"],function(e){e.lazy.controller("CatalogController",["$routeParams","$scope","$location","ProductService",function(e,t,n,r){t.init=function(){t.type=e.type;var i;if(angular.isUndefined(t.type)){i="products.json"}else{i="products/"+t.type+".json"}r.getProducts(i).success(function(e){t.products=e.products}).error(function(){console.log("error");n.path("/index")})};t.buildSmallImg=function(e){if(angular.isUndefined(e)){return}else{return"img/products/small/"+t.type+"/"+e+".jpg"}};t.buildLargeImg=function(e){if(angular.isUndefined(e)){return}else{return"img/products/large/"+t.type+"/"+e+".jpg"}};t.init()}])})
+define(["appModule"], function (e) {
+    e.lazy.controller("CatalogController", ["$routeParams", "$scope", "$location", "ProductService",
+        function (e, t, n, r) {
+            t.init = function () {
+                t.type = e.type;
+                var i;
+                if (angular.isUndefined(t.type)) {
+                	n.path("/index")
+                } else {
+                    i = "products/" + t.type + ".json"
+                }
+                r.getProducts(i).success(function (e) {
+                    t.products = e.products
+                }).error(function () {
+                    console.log("error");
+                    n.path("/index")
+                })
+            };
+            t.buildSmallImg = function (e) {
+                if (angular.isUndefined(e)) {
+                    return
+                } else {
+                    return "img/products/small/" + t.type + "/" + e + ".jpg"
+                }
+            };
+            t.buildLargeImg = function (e) {
+                if (angular.isUndefined(e)) {
+                    return
+                } else {
+                    return "img/products/large/" + t.type + "/" + e + ".jpg"
+                }
+            };
+            t.init()
+        }
+    ])
+})
